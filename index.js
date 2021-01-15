ad473f9c22a36a5315b9f49f889fadc971db5f03
wnortier <warwick.nortier@gmail.com>
2021-01-15 13:13:39 +0200

    added media queries

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
     else myVideo.pause();
   }
 
+  console.log(document);
+
   playPause();
+  window.addEventListener("resize", () => {
+    console.log(window.innerWidth);
+    if (window.innerWidth <= 500) {
+      document.querySelector(".content__heading").innerHTML = "Chance";
+    } else {
+      document.querySelector(".content__heading").innerHTML =
+        "The Sky Is The Limit";
+    }
+  });
 });
